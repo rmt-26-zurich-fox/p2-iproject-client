bc33e7b4001b817c48dba2b6f6a60ebb63d0c806
rysaldiwijaya <rysaldii@gmail.com>
2022-08-17 03:08:39 +0700

    init vue build tools and setup

--- client/src/main.js
+++ client/src/main.js
@@ -4,9 +4,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// Import our custom CSS
+import './assets/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 const app = createApp(App)
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+  store.router = markRaw(router)
+})
 
 app.use(createPinia())
 app.use(router)

e1bd9dc14479f7d789f458c7c821640b4d32f977
inyund <mfrafie@gmail.com>
2022-08-16 13:36:20 +0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { createApp, markRaw } from "vue";
+import { createPinia } from "pinia";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(({ store }) => {
+  store.router = markRaw(router);
+});
+const options = {
+  // You can set your default options here
+};
+
+app.use(pinia);
+app.use(router);
+app.use(Toast, options);
+
+app.mount("#app");
---
+++ src/stores/wyr.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+import wyr from "../apis/wyrApi";
+
+export const useWyrStore = defineStore({
+  id: "wyr",
+  state: () => ({
+    // some state
+  }),
+  actions: {
+    // some actions
+  },
+});

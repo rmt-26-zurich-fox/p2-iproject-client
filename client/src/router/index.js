b865657f196c4902f01cf78cb2044a66415d7254
erlanggakpp <erlanggakencana@gmail.com>
2022-08-17 09:37:44 +0700

    Few adjustment

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "../views/LoginPage.vue";
+import RegisterPage from "../views/RegisterPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/login",
       name: "LoginPage",
       component: LoginPage,
+    },
+    {
+      path: "/register",
+      name: "RegisterPage",
+      component: RegisterPage,
     },
     // {
     //   path: "/about",

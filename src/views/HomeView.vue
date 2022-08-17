2e59025b452db36d9f610f989172f9b6ebffed2b
Dhon23 <m.ramadhan.ak@gmail.com>
2022-08-17 22:56:30 +0700

    moving fetch data on movieList to homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,49 @@
 <template>
   <MovieAll />
-  <MovieList title="Trending" />
-  <MovieList title="Top Vote" />
+  <MovieList :data="trending" title="Trending" />
+  <MovieList :data="topVote" title="Top Vote" />
 </template>
 
 <script>
+import axios from "axios";
+import { mapState, mapActions } from "pinia";
+import { useGlobalStore } from "../stores/globalStore";
 import MovieAll from "../components/MovieAll.vue";
 import MovieList from "../components/MovieList.vue";
 
 export default {
   components: { MovieAll, MovieList },
+  data() {
+    return {
+      trending: [],
+      topVote: [],
+    };
+  },
+  computed: {
+    ...mapState(useGlobalStore, ["baseUrl"]),
+  },
+  methods: {
+    ...mapActions(useGlobalStore, ["errorHandler"]),
+    async movieTrending() {
+      try {
+        const { data } = await axios.get(this.baseUrl + "/movies/trending");
+        this.trending = data.results;
+      } catch (error) {
+        this.errorHandler(error);
+      }
+    },
+    async movieTopVote() {
+      try {
+        const { data } = await axios.get(this.baseUrl + "/movies/top-vote");
+        this.topVote = data.results;
+      } catch (error) {
+        this.errorHandler(error);
+      }
+    },
+  },
+  created() {
+    this.movieTrending();
+    this.movieTopVote();
+  },
 };
 </script>

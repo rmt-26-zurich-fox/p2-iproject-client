1cdc4253e25bc58d10035f7c618dad05dc82b0b6
HafizhIslami <hafizhislami11@gmail.com>
2022-08-17 11:44:02 +0700

    add product store, add fetch data product, and add request product

---
+++ client/src/stores/product.js
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+
+export const useProductStore = defineStore({
+  id: "product",
+  state: () => ({
+    productsList: [],
+  }),
+  actions: {
+    async fetchDataProduct() {
+      try {
+        const product = await axios.get(`${baseUrl}/product`, {
+          headers: { access_token: localStorage.getItem("access_token") },
+        });
+        this.productList = product.data;
+      } catch (error) {
+        console.log(error);
+        Swal.fire(error.response.data.message)
+      }
+    },
+    async fetchRequestProduct() {
+      try {
+        const product = await axios.get(`${baseUrl}/request/product/:productId`, {
+          headers: { access_token: localStorage.getItem("access_token") },
+        });
+        this.productList = product.data;
+      } catch (error) {
+        console.log(error);
+        Swal.fire(error.response.data.message)
+      }
+    },
+  },
+});

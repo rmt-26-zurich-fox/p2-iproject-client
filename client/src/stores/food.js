eadebf54770a5ead29fceeee23a759c24b872f47
jordxx <danieljordan553@gmail.com>
2022-08-17 23:36:30 +0700

    Feat: deployment

--- client/src/stores/food.js
+++ client/src/stores/food.js
@@ -5,7 +5,8 @@ import Swal from "sweetalert2"
 export const useFoodStore = defineStore({
   id: "food",
   state: () => ({
-    host: "http://localhost:3000",
+    // host: "http://localhost:3000",
+    host: "https://dieat-server.herokuapp.com",
     foods: {},
     bmi: {},
     bmr: {},

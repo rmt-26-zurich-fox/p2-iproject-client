de5270005fc1a50d1fa36531f529686979dc07a1
Ghozi Fattah Allauddin <ghozifattah@gmail.com>
2022-08-17 13:06:59 +0700

    component dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script>
 import { RouterView } from "vue-router";
+import { useCounterStore } from "./stores/counter";
+import { mapWritableState, mapActions } from "pinia";
+
+export default {
+  methods: {
+    ...mapActions(useCounterStore, ["parseJwt"])
+  },  
+  computed: {
+    ...mapWritableState(useCounterStore, ["isLogin", "username"])
+  },
+  created () {
+    if (localStorage.getItem("access_token")) {
+      const payload = this.parseJwt(localStorage.getItem("access_token"));
+      this.username = payload.username;
+      this.isLogin = true;
+    }
+  }
+}
 </script>
 
 <template>

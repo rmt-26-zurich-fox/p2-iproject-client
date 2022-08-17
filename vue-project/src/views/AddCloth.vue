1a7fc3290268b25ac965691464e212047bbf4a52
AzmKml <azmkml@gmail.com>
2022-08-18 06:02:07 +0700

    slicing component and pages, handle login, register, added feature: create Laundry, edit laundry, midtrans payment

---
+++ vue-project/src/views/AddCloth.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="add-food">
+    <div class="d-flex justify-content-center">
+      <div class="card shadow" style="margin-top: 100px; width: 25rem">
+        <div class="card-body">
+          <FormCloth />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FormCloth from "../components/FormCloth.vue";
+
+export default {
+  components: {
+    FormCloth,
+  },
+};
+</script>

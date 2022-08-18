72edb3d5317d984e30a6fcf276c4f44b9de3e97a
Rio Hanantyoko <rhanantyoko@gmail.com>
2022-08-18 11:02:24 +0700

    Sebelum deploy

--- theSouthFace/vite.config.js
+++ theSouthFace/vite.config.js
@@ -5,24 +5,24 @@ import vue from '@vitejs/plugin-vue'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
-// export default defineConfig(({ command }) => {
-//   return {
-//     plugins: [vue()],
-//     resolve: {
-//       alias: [
-//         {
-//           find: "@vue/runtime-core",
-//           replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js",
-//         },
-//       ],
-//     },
-//   };
-// });
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: [
+        {
+          find: "@vue/runtime-core",
+          replacement: "@vue/runtime-core/dist/runtime-core.esm-bundler.js",
+        },
+      ],
+    },
+  };
+});

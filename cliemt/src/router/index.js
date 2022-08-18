64f54dc8ca881c0e5ab2d956d26018a0e68160ef
SyahidbinFadlyl <ava.rageman18@gmail.com>
2022-08-18 10:25:39 +0700

    Add route guard

--- cliemt/src/router/index.js
+++ cliemt/src/router/index.js
@@ -45,7 +45,7 @@ router.beforeEach((to, from, next) => {
   const isLogin = localStorage.access_token;
   if ((to.name === "login" || to.name === "register") && isLogin) {
     next({ name: "home" });
-  } else if ((to.name === "home" || to.name === "profile") && !isLogin) {
+  } else if ((to.name === "home" || to.name === "profile" || to.name === "new-post" || to.name === "post_detail") && !isLogin) {
     Swal.fire({
       icon: "error",
       title: "Oops...",

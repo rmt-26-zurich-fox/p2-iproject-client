faa0e6258c0d3d2a0156b2b2328ade978b8233a4
Dhon23 <m.ramadhan.ak@gmail.com>
2022-08-19 07:33:55 +0700

    add donate feature with midtrans

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -4,9 +4,75 @@
       <img src="../assets/tmdb-logo.svg" />
     </div>
     <div></div>
-    <div><button>Donate</button></div>
+    <div><button @click="donate">Donate</button></div>
   </footer>
 </template>
+
+<script>
+import axios from "axios";
+import Swal from "sweetalert2";
+import { mapState, mapActions } from "pinia";
+import { useGlobalStore } from "../stores/globalStore";
+
+export default {
+  computed: {
+    ...mapState(useGlobalStore, ["baseUrl"]),
+  },
+  methods: {
+    ...mapActions(useGlobalStore, ["errorHandler", "successHandler"]),
+    async donate() {
+      try {
+        const errorHandler = this.errorHandler;
+        const access_token = localStorage.access_token;
+
+        const { value: amount } = await Swal.fire({
+          title: "Enter amount you want to donate",
+          input: "number",
+          inputLabel: "Your amount",
+          showCancelButton: true,
+          icon: "question",
+          inputValidator: (value) => {
+            if (!value) {
+              return "You need to write something!";
+            }
+          },
+        });
+
+        if (!amount) {
+          throw { message: "Canceled" };
+        }
+
+        const { data } = await axios.post(
+          this.baseUrl + "/payment",
+          {
+            amount,
+          },
+          {
+            headers: {
+              access_token,
+            },
+          }
+        );
+
+        window.snap.pay(data.transactionToken, {
+          onSuccess(result) {
+            console.log(result);
+          },
+          onClose() {
+            errorHandler("You closed the popup without finishing the payment");
+          },
+        });
+      } catch (error) {
+        if (error.response) {
+          if (error.response.status === 401)
+            return this.errorHandler("Login require");
+        }
+        this.errorHandler(error.message);
+      }
+    },
+  },
+};
+</script>
 
 <style>
 footer img {
